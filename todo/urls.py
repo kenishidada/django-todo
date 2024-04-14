e3d78a5f70e698ad8ab6d4910439080a0c4f0e1f
KenIshida <kenishida95@gmail.com>
2024-04-14 13:50:14 +0900

    [update]add crud

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
+from .views import TodoList, TodoDetail, TodoCreate, TodoDelete
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', TodoList.as_view(), name='list'),
+    path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
+    path('create/', TodoCreate.as_view(), name='create'),
+    path('delete/<int:pk>', TodoDelete.as_view(), name='delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from .models import TodoModel
 
 # Create your views here.
+
+class TodoList(ListView):
+    template_name = 'list.html'
+    model = TodoModel
+    
+
+class TodoDetail(DetailView):
+    template_name = 'detail.html'
+    model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('__all__')
+    success_url = reverse_lazy('list')
+    
+    
+class TodoDelete(DeleteView):
+    template_name = "delete.html"
+    model = TodoModel
+    success_url = reverse_lazy('list')
